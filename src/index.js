452fc7d33288bd5fdef0e0302f2cc56d7a317512
HoangHuy <hoanghuy226@HoangHuys-MacBook-Pro.local>
2019-03-13 15:45:02 +0700

    init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const codec = require('./icetea-common/codec')
+const ecc = require('./icetea-common/ecc')
+const Tx = require('./icetea-common/Tx')
+
+exports.codec = codec
+exports.ecc = ecc
+exports.Tx = Tx
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+
+const path = require('path');
+const webpack = require('webpack');
+const packageJson = require('./package');
+
+const outDir = process.env.RUNTYPER ? '.runtyper' : 'dist';
+const runtyper = process.env.RUNTYPER ? ['babel-plugin-runtyper', {
+  warnLevel: 'break',
+  implicitAddStringNumber: 'allow',
+}] : null;
+const babelPlugins = [runtyper].filter(Boolean);
+const outFile = path.basename(packageJson.main);
+
+module.exports = {
+  mode: 'development',
+  entry: './src',
+  output: {
+    path: path.resolve(outDir),
+    filename: outFile,
+    libraryTarget: 'umd',
+    library: 'IceTeaWeb3',
+    globalObject: 'this', // https://github.com/webpack/webpack/issues/6525
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: babelPlugins,
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.BannerPlugin(`${packageJson.name} v${packageJson.version}`)
+  ]
+};
