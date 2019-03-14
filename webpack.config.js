7056327919c633e1bd146b480751b2733b4a13b2
Truong Hong Thi <thi1981@gmail.com>
2019-03-14 18:47:28 +0700

    Support both browser and node

--- webpack.config.js
+++ webpack.config.js
@@ -3,41 +3,50 @@ const path = require('path');
 const webpack = require('webpack');
 const packageJson = require('./package');
 
+const nodeExternals = require('webpack-node-externals');
+
 const outDir = process.env.RUNTYPER ? '.runtyper' : 'dist';
 const runtyper = process.env.RUNTYPER ? ['babel-plugin-runtyper', {
   warnLevel: 'break',
   implicitAddStringNumber: 'allow',
 }] : null;
 const babelPlugins = [runtyper].filter(Boolean);
-const outFile = path.basename(packageJson.main);
 
-module.exports = {
-  mode: 'development',
-  entry: './src',
-  output: {
-    path: path.resolve(outDir),
-    filename: outFile,
-    libraryTarget: 'umd',
-    library: 'IceTeaWeb3',
-    globalObject: 'this', // https://github.com/webpack/webpack/issues/6525
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: babelPlugins,
+function makeConfig(target) {
+  const isNode = target === 'node'
+  const outFile = isNode ? path.basename(packageJson.main) : path.basename(packageJson.browser)
+  return {
+    target: target,
+    externals: isNode ? [nodeExternals()] : undefined,
+    mode: 'development',
+    entry: './src',
+    output: {
+      path: path.resolve(outDir),
+      filename: outFile,
+      libraryTarget: 'umd',
+      library: 'IceTeaWeb3',
+      globalObject: 'this', // https://github.com/webpack/webpack/issues/6525
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: babelPlugins,
+            }
           }
         }
-      }
+      ]
+    },
+    plugins: [
+      new webpack.BannerPlugin(`${packageJson.name} v${packageJson.version}`)
     ]
-  },
-  plugins: [
-    new webpack.BannerPlugin(`${packageJson.name} v${packageJson.version}`)
-  ]
+  }
 };
+
+module.exports = [makeConfig('node'), makeConfig('web')]
